038c7e0a8db2714fd4c9b5e8e98f7a10523b1a13
ConradoEC <erickconrado317@gmail.com>
2025-04-24 05:19:33 -0300

    uploads #6

--- connection/connectiondb.js
+++ connection/connectiondb.js
@@ -2,7 +2,6 @@ const mongodb = require("mongodb")
 const mongoose = require("mongoose")
 
 const connectiondb = async() => {
-    console.log(process.env.DB_USERNAME)
     await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@testedechatws.arahusq.mongodb.net/?retryWrites=true&w=majority&appName=testeDeChatWS`)
     .then(() => {
         console.log("DB connected")
